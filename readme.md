794f2c0f40582e8a9266c2a793f7116d42cbaf06
shahrooz <shahroozsanei@gmail.com>
2024-07-30 23:30:06 +0330

    init

---
+++ readme.md
@@ -0,0 +1,88 @@
+
+## فارسی
+[برو به انگلیسی](#english)
+
+### تمرین استخدامی
+به تمرین استخدامی ما خوش آمدید. در این تمرین، ما یک کتابخانه ساده ایجاد کرده‌ایم که از React الهام گرفته شده است. هدف شما این است که قالب کد ارائه‌شده را تحلیل کرده و تابع `useEffect` را تکمیل کنید. این تابع باید اطمینان حاصل کند که تابع داخلی آن بر اساس وابستگی‌های مشخص شده در زمان اجرای برنامه به درستی اجرا می‌شود.
+
+شما می‌توانید قالب کد را در فایل [task.js](task.js) پیدا کنید.
+
+### دستورالعمل‌ها
+1. **تحلیل** قالب کد ارائه‌شده.
+2. **تکمیل** تابع `useEffect` در `task.js`.
+3. اطمینان حاصل کنید که تابع داخلی `useEffect` بر اساس وابستگی‌های مشخص شده به درستی اجرا می‌شود.
+
+### راه‌اندازی و نصب
+برای شروع پروژه، مراحل زیر را دنبال کنید:
+
+1. مخزن را کلون کنید:
+    ```sh
+    git clone https://github.com/ShahroozD/Recruitment-Task
+    cd Recruitment-Task
+    ```
+
+2. وابستگی‌های مورد نیاز را نصب کنید:
+    ```sh
+    npm install
+    ```
+
+3. پکیج TypeScript را نصب کنید:
+    ```sh
+    npm install -D typescript
+    ```
+
+4. دستور زیر را اجرا کنید تا کدهای شما کامپایل شود:
+    ```sh
+    npm run dev
+    ```
+
+5. سرور توسعه را راه‌اندازی کنید:
+    ```sh
+    npx serve .
+    ```
+
+
+---
+
+## English
+[Go to Persian](#فارسی)
+
+### Recruitment Task
+Welcome to our recruitment task. In this exercise, we have created a simplified library inspired by React. Your objective is to analyze the provided code template and complete the `useEffect` function. This function should ensure that its inner function executes correctly based on its dependencies when the program runs.
+
+You can find the code template in the [task.js](task.js) file.
+
+### Instructions
+1. **Analyze** the provided code template.
+2. **Complete** the `useEffect` function in `task.js`.
+3. Ensure that the inner function of `useEffect` executes according to the specified dependencies.
+
+### Setup and Installation
+To get started with the project, follow these steps:
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/ShahroozD/Recruitment-Task
+    cd Recruitment-Task
+    ```
+
+2. Install the required dependencies:
+    ```sh
+    npm install
+    ```
+
+3. (Optional) If you want to use TypeScript, install it as a development dependency:
+    ```sh
+    npm install -D typescript
+    ```
+
+4. Start the development env:
+    ```sh
+    npm run dev
+    ```
+
+5. Serve the project:
+    ```sh
+    npx serve .
+    ```
+
---
+++ task.tsx
@@ -0,0 +1,122 @@
+
+// ---- Library --- //
+const appState = [];
+let appStateCursor = 0;
+
+
+const React = {
+    root : null,
+    
+    render: (el, container)=>{
+        React.root = container;
+        render(el, container)
+    },
+
+    createElement: (tag, props, ...children) => {
+        if (typeof tag === 'function') {
+            return tag(props, ...children);
+        }
+        const el = {
+            tag,
+            props,
+            children,
+        };
+        return el;
+    },
+};
+
+const render = (el, container) => {       
+    let domEl;
+    if (typeof el === 'string' || typeof el === 'number') {
+        domEl = document.createTextNode(String(el));
+        container.appendChild(domEl);
+        return;
+    }
+
+    domEl = document.createElement(el.tag);
+    // افزودن ویژگیهای تگ
+    let elProps = el.props ? Object.keys(el.props) : null;
+    if (elProps && elProps.length > 0) {
+        elProps.forEach((prop) => (domEl[prop] = el.props[prop]));
+    }
+    // افزودن فرزندان داخلی تگ
+    if (el.children && el.children.length > 0) {
+        el.children.forEach((node) => render(node, domEl));
+    }
+    container.appendChild(domEl);
+};
+
+const reRender = () => {
+    console.log('reRendering :)');
+    // پاک کردن و رسم دوباره
+    React.root.innerHTML = '';
+    appStateCursor = 0;
+    render(<App />, React.root);
+
+
+};
+  
+
+const useState = (initialState) => {
+    // بر اساس اشاره‌گر، متغییر در استک قرار میگیرد
+    const stateCursor = appStateCursor;
+    appState[stateCursor] = appState[stateCursor] || initialState;
+
+    const setState = (newState) => {
+
+        appState[stateCursor] = newState;
+        // با تغییر متغییر صفحه دوباره رسم میشود
+        reRender();
+    };
+
+    appStateCursor++;
+
+    return [appState[stateCursor], setState];
+};
+
+const useEffect = (callback, conditions = []) => {
+    // وظیفه شما: این تابع راتکمیل کنید
+    // your task: complete this function
+};
+
+
+
+// ---- Application --- //
+const App = () => {
+    const [name, setName] = useState('Developer');
+    const [count, setCount] = useState(0);
+    const [final, setFinal] = useState("Do somthing here!");
+
+    useEffect(() => {
+        setFinal(`useEffect response!`);
+    }, [count]);
+
+
+    return (
+        <div style="display: grid; gap: 10px; justify-content: center;text-align: center;">
+            <div>
+                <h2>Hello {name}!</h2>
+                <p>Enter Your Name:</p>
+                <input
+                    type="text"
+                    value={name}
+                    onchange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div >
+                <h2> Counter value: {count}</h2>
+                <div style="display: flex; gap: 10px;justify-content: center;">
+                    <button onclick={() => setCount(count + 1)}>+1</button>
+                    <button onclick={() => setCount(count - 1)}>-1</button>
+                </div>
+            </div>
+            <div style="padding: 10px; background: #ccc">
+                <h2>{final}</h2>
+            </div>
+        </div>
+    );
+};
+
+
+// ---- Application --- //
+React.render(<App /> , document.getElementById('app'));
